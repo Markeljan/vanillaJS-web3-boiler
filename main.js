63d390393e4b9e02077d23ff5b64cc01f9b5472e
Markeljan <Markeljan19@gmail.com>
2022-06-03 12:47:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ function init() {
         if(ethereum.isMetaMask && localStorage.getItem("CACHED_PROVIDER") === "TRUE") {
             fetchAccountData();
         };
-
     } catch (error) {
         console.log("Error connecting to metamask account:\n", error)
         if (window.confirm("Install Metamask to access Web3 Content. \nClick OK to be directed to metamask.io ")) {
-          window.open("http://metamask.io", "_blank");
-        };
+            window.open("http://metamask.io", "_blank");
+            };
         }
 };
 
@@ -19,13 +18,13 @@ async function fetchAccountData() {
     try {
         const provider = new ethers.providers.Web3Provider(ethereum);
         let account = await provider.send("eth_requestAccounts").then( accounts => {
-          return accounts[0];
-        });
+          return accounts[0];});
         let balance = await provider.getBalance(account);
         let formatedBalance = ethers.BigNumber.from(balance);
         formatedBalance = balance.mod(1e14);
         formatedBalance = ethers.utils.formatEther(balance.sub(formatedBalance));
-        //updateHTMLElements
+        
+        //updateHTMLElements network/balances/button
         document.getElementById("selected-account").innerHTML = `${account}`;
         document.getElementById("account-balance").innerHTML = ` - ${formatedBalance} ${chainIdMap[ethereum.networkVersion].symbol}`;
         document.getElementById("network-name").innerHTML = `${chainIdMap[ethereum.networkVersion].name}`;
@@ -34,18 +33,16 @@ async function fetchAccountData() {
         localStorage.setItem("CACHED_PROVIDER", "TRUE");
     } catch (error) {
         console.log("Error connecting to metamask account:\n", error)
-    }
+      }
 
   ethereum.on("accountsChanged", (accounts) => {
       if(accounts[0]) {
         fetchAccountData();
-        
       } else {
         localStorage.removeItem("CACHED_PROVIDER");
         document.getElementById("btn-disconnect").style.display = "none";
         document.getElementById("btn-connect").style.display = "block";
       }
-
   });
   ethereum.on("chainChanged", (chainId) => {
     fetchAccountData();
