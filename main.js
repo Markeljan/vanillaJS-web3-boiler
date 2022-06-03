df2796726a0fe473ad5dbb2d13427e4b93bde9c5
Markeljan <Markeljan19@gmail.com>
2022-06-03 00:41:46 -0400

    boilerplateUpdates

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
+let provider;
+let signer;
+let account;
 
+document.getElementById("btn-connect").addEventListener("click", fetchAccountData)
+document.getElementById("btn-disconnect").addEventListener("click", onDisconnect)
 
 function init() {
+    try {
+        if(ethereum.isMetaMask && localStorage.getItem("CACHED_PROVIDER") === "TRUE") {
+            fetchAccountData();
+        };
+
+    } catch (error) {
+        console.log("Error connecting to metamask account:\n", error)
+        if (window.confirm("Install Metamask to access Web3 Content. \nClick OK to be directed to metamask.io ")) {
+          window.open("http://metamask.io", "_blank");
+        };
+        }
+};
     
-}
-
-async function fetchAccountData() {}
-
 async function refreshAccountData() {}
 
+async function fetchAccountData() {
+    try {
+        account = await ethereum.request({method: "eth_requestAccounts"});
+        document.getElementById("btn-connect").style.display = "none";
+        document.getElementById("btn-disconnect").style.display = "block";
+        localStorage.setItem("CACHED_PROVIDER", "TRUE");
+    } catch (error) {
+        console.log("Error connecting to metamask account:\n", error)
+    }
 
-async function onConnect() {}
+  ethereum.on("accountsChanged", (accounts) => {
+      if(accounts[0]) {
+        fetchAccountData();
+        
+      } else {
+        console.log("no account");
+        localStorage.removeItem("CACHED_PROVIDER");
+        document.getElementById("btn-disconnect").style.display = "none";
+        document.getElementById("btn-connect").style.display = "block";
+      }
 
-async function onDisconnect() {}
+  });
+  ethereum.on("chainChanged", (chainId) => {
+    fetchAccountData();
+  });
+};
+
+function onDisconnect() {
+    alert("To disconnect, open MetaMask and manualy disconnect.")
+}
 
 window.addEventListener('load', async () => {
     init();
-    if(localStorage.getItem("WEB3_CONNECT_CACHED_PROVIDER")) await onConnect();
-    document.querySelector("#btn-connect").addEventListener("click", onConnect);
-    document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
-  
-    document.querySelector("#radio-mumbai").addEventListener("click", function () {switchNetworkMumbai();});
-    document.querySelector("#radio-polygon").addEventListener("click", function () {switchNetworkPolygon();}); 
   });
